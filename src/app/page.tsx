103700f0792d91e08c09407bd1826581addf9578
Purusotam Mishra <91256718+PurusotamMishra@users.noreply.github.com>
2024-08-16 00:14:26 +0530

    Weather app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,177 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import styles from './page.module.css';
+import { useEffect, useState } from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import CloudIcon from '@mui/icons-material/Cloud';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import DehazeIcon from '@mui/icons-material/Dehaze';
+import MasksIcon from '@mui/icons-material/Masks';
+
+let WEATHER_API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY
+
+
+
+function Clock({ targetTimeZoneOffset }: any): JSX.Element {
+  const [currentTime, setCurrentTime] = useState(new Date(Date.now() + targetTimeZoneOffset));
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date(Date.now() + targetTimeZoneOffset));
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [targetTimeZoneOffset]);
+
+  return <p className={styles.time}>{currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', timeZone: 'UTC' })}</p>;
+}
+
 
 export default function Home() {
+  const [place, setPlace] = useState("Mumbai");
+  const [placeData, setPlaceData] = useState<any>(null)
+
+  const getWeatherData = async () => {
+    //# https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=WEATHER_API_KEY
+
+    if(place && place.length >0){
+      try{
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${WEATHER_API_KEY}`;
+
+        let res = await fetch(url); //response
+        let data = await res.json();
+        console.log("GET WEATHER DATA RESPONSE",data);
+        setPlaceData(data)
+      }
+      catch(err){
+        console.log(err)
+      }
+    }
+    
+  }
+  
+  useEffect(() => {
+    getWeatherData();
+  }, [])
+
+ 
+  const targetTimeZoneOffset = placeData?.timezone * 1000; //miliseconds 
+
+  const currentDate = new Date(Date.now() + targetTimeZoneOffset).toLocaleDateString([], {
+    weekday: 'short', month: 'short', day: 'numeric', timeZone: 'UTC'
+  })
+  
+  const currentTime = new Date(Date.now() + targetTimeZoneOffset).toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    // second: '2-digit',
+    timeZone: 'UTC'
+  })
+  
+  //background images
+  // const Temper = placeData?.main.temp;
+  const getTemperatureClass = (Temperature : number) => {
+    Temperature -= 273.15;
+    // if(!Temper){
+    //   return ''
+    // }
+    if (Temperature < 0) {
+      return styles.coldest;
+    } else if (Temperature >= 0 && Temperature < 15) {
+      return styles.cold;
+    } else if (Temperature >= 15 && Temperature < 30) {
+      return styles.clear;
+    } else if (Temperature >= 30 && Temperature < 45) {
+      return styles.warm;
+    }else {
+      return styles.hot;
+    }
+  };
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className={`${styles.home} ${getTemperatureClass(placeData?.main.temp)}`}>
+      <div className={styles.searchBar}>
+        <input type= "search" placeholder="City Name" onChange={(e) => setPlace(e.target.value)} />
+        <button onClick={getWeatherData}> <SearchIcon /> </button>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+    {
+      placeData && <div className={styles.row}>
+      <div className={styles.section1}>
+        <div className={styles.section11}>
+          {placeData.weather[0].main === 'Clouds' && 
+          <CloudIcon className={styles.weatherIcon} />
+          }
+          {placeData.weather[0].main === 'Clear' && 
+          <WbSunnyIcon className={styles.weatherIcon} />
+          }
+          {placeData.weather[0].main === 'Haze' && 
+          <DehazeIcon className={styles.weatherIcon} />
+          }
+          {placeData.weather[0].main === 'Smoke' && 
+          <MasksIcon className={styles.weatherIcon} />
+          }
+          <p className={styles.temp}>
+            {(placeData?.main.temp - 273.15).toFixed(1)} <span> °C </span>
           </p>
-        </a>
+          
+        </div>
+        <div className={styles.section11}>
+          <p className={styles.city}> {placeData?.name} </p>
+          <p className={styles.weatherType}> {placeData?.weather[0].main} </p>
       </div>
-    </main>
+      </div>
+      <div className={styles.timediv}>
+          <p className={styles.time}>{currentDate}</p><Clock targetTimeZoneOffset={targetTimeZoneOffset} />
+      </div>
+      </div>
+    }
+
+    {
+      placeData && 
+      <div className={styles.section2}>
+
+        <div className={styles.section21}>
+          <p className={styles.head1}>Feels Like</p>
+          <p className={styles.head2}>{(placeData?.main.feels_like - 273.15).toFixed(1)} °C</p>
+        </div>
+
+        <div className={styles.section21}>
+          <p className={styles.head1}>Min Temperature</p>
+          <p className={styles.head2}>{(placeData?.main.temp_min - 273.15).toFixed(1)} °C</p>
+        </div>
+
+        <div className={styles.section21}>
+          <p className={styles.head1}>Max Temperature</p>
+          <p className={styles.head2}>{(placeData?.main.temp_max - 273.15).toFixed(1)} °C</p>
+        </div>
+
+        <div className={styles.section21}>
+          <p className={styles.head1}>Humidity</p>
+          <p className={styles.head2}>{(placeData?.main.humidity).toFixed(1)} %</p>
+        </div>
+
+        <div className={styles.section21}>
+          <p className={styles.head1}>Pressure</p>
+          <p className={styles.head2}>{(placeData?.main.pressure)} hPa</p>
+        </div>
+
+        <div className={styles.section21}>
+          <p className={styles.head1}>Visibility</p>
+          <p className={styles.head2}>{(placeData?.visibility)} m</p>
+        </div>
+
+        <div className={styles.section21}>
+          <p className={styles.head1}>Wind Speed</p>
+          <p className={styles.head2}>{(placeData?.wind.speed)} m/s</p>
+        </div>
+
+      </div>
+    }
+
+
+    </div>
   );
 }
